8d5b6bf4d2e2dd98acc9f6de0cf6a83da26579c9
qian944 <qianxunliu48@gmail.com>
2025-06-10 17:32:06 +0800

    Update plot_utils.py

--- plot_utils.py
+++ plot_utils.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 def plot_stock(df):
     fig, ax = plt.subplots()
     ax.plot(df['trade_date'], df['close'], marker='o')
-    ax.set_title("近一周股价")
-    ax.set_xlabel("日期")
+    ax.set_title("近一周股价走势")
+    ax.set_xlabel("交易日期")
     ax.set_ylabel("收盘价")
     plt.xticks(rotation=45)
     return fig
