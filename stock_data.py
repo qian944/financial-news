594fedddcde3facd1a3432a1b31c2cf0743cfc8a
qian944 <qianxunliu48@gmail.com>
2025-06-10 17:31:43 +0800

    Update stock_data.py

--- stock_data.py
+++ stock_data.py
@@ -1,18 +1,17 @@
 import tushare as ts
 import pandas as pd
-from settings import TUSHARE_TOKEN
+import streamlit as st
 
-ts.set_token(TUSHARE_TOKEN)
+ts.set_token(st.secrets["TUSHARE_TOKEN"])
 pro = ts.pro_api()
 
 def get_stock_data(stock_code, pub_date):
     start = pd.to_datetime(pub_date) - pd.Timedelta(days=3)
     end = pd.to_datetime(pub_date) + pd.Timedelta(days=3)
-    start_str = start.strftime("%Y%m%d")
-    end_str = end.strftime("%Y%m%d")
-
     try:
-        hist = ts.pro_bar(ts_code=stock_code, start_date=start_str, end_date=end_str)
-        return hist.sort_values('trade_date')
-    except Exception:
+        df = ts.pro_bar(ts_code=stock_code,
+                        start_date=start.strftime('%Y%m%d'),
+                        end_date=end.strftime('%Y%m%d'))
+        return df.sort_values("trade_date") if df is not None else None
+    except:
         return None
